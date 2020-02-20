528949446deab1920f4f33c81a6354e896c48d76
daodao1996 <yunpeng.ding@thoughtworks.com>
2020-02-20 21:11:24 +0800

    refactor getDateDetail method by String.format

--- src/main/java/cc/xpbootcamp/warmup/cashier/Order.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/Order.java
@@ -25,15 +25,11 @@ public class Order {
     }
 
     String getDateDetail(){
-        StringBuilder output = new StringBuilder();
         String[] weeks = {"星期日","星期一","星期二","星期三","星期四","星期五","星期六"};
 
-        output.append(orderTime.get(Calendar.YEAR)).append("年");
-        output.append(orderTime.get(Calendar.MONTH) + 1).append("月");
-        output.append(orderTime.get(Calendar.DATE)).append("日, ");
-        output.append(weeks[orderTime.get(Calendar.DAY_OF_WEEK) - 1]).append("\n");
-
-        return output.toString();
+        return String.format("%d年%d月%d日, %s\n", orderTime.get(Calendar.YEAR),
+                orderTime.get(Calendar.MONTH) + 1, orderTime.get(Calendar.DATE),
+                weeks[orderTime.get(Calendar.DAY_OF_WEEK) - 1]);
     }
 
     public String toString() {
